dbb155d13e4941fe52782271dd7ddd028310e067
chris <contact@chrisdunne.net>
2020-05-17 00:00:15 +0100

    add list overload to get

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='cd-hibp',  
-    version='1.0.0',
+    version='1.0.1',
     author="Christopher Dunne",
     author_email="contact@chrisdunne.net",
     description="A package for utilising the Haveibeenpwned API",
@@ -16,5 +16,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    #install_requires  = [],
+    install_requires  = ["requests"],
  )
