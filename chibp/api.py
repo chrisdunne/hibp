34eda4fe186cf5827048dc9ae10156e8b56e04db
chris <contact@chrisdunne.net>
2020-05-19 11:53:27 +0100

    return response text instead of response

--- chibp/api.py
+++ chibp/api.py
@@ -6,7 +6,7 @@ def get(email, key):
         return requests.get(
             f"https://haveibeenpwned.com/api/v3/breachedaccount/{urllib.parse.quote(email)}",
             headers={"hibp-api-key": key}  
-        )
+        ).text
     
     if isinstance(email, list):
         result = {}
@@ -15,7 +15,7 @@ def get(email, key):
                 f"https://haveibeenpwned.com/api/v3/breachedaccount/{urllib.parse.quote(e)}",
                 headers={"hibp-api-key": key}  
             )
-            result[e] = r
+            result[e] = r.text
         return result
     
 def get_full(email, key):
@@ -23,7 +23,7 @@ def get_full(email, key):
         return requests.get(
             f"https://haveibeenpwned.com/api/v3/breachedaccount/{urllib.parse.quote(email)}?truncateResponse=false",
              headers={"hibp-api-key": key}
-        )
+        ).text
 
     if isinstance(email, list):
         result = {}
@@ -32,23 +32,23 @@ def get_full(email, key):
                 f"https://haveibeenpwned.com/api/v3/breachedaccount/{urllib.parse.quote(e)}?truncateResponse=false",
                 headers={"hibp-api-key": key}
             )
-            result[e] = r
+            result[e] = r.text
         return result
 
 def get_breaches(key):
     return requests.get(
         f"https://haveibeenpwned.com/api/v3/breaches",
         headers={"hibp-api-key": key}
-    )
+    ).text
 
 def get_breach(site, key):
     return requests.get(
         f"https://haveibeenpwned.com/api/v3/breach/{site}",
         headers={"hibp-api-key": key}
-    )
+    ).text
 
 def get_pastes(email, key):
     return requests.get(
         f"https://haveibeenpwned.com/api/v3/pasteaccount/{urllib.parse.quote(email)}",
         headers={"hibp-api-key": key}
-    )
+    ).text
